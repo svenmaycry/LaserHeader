4f7edf39d21cf9987642a19d7d5a0a7df1dce79a
SvenMayCry <svenmaycry@gmail.com>
2024-02-26 15:25:57 +0300

    26.02 - Работа с адаптивом. Элементам меню добавлена бегущая строка при клике.

--- src/js/files/script.js
+++
@@ -1,8 +0,0 @@
-// const searchButton = document.getElementById('searchId');
-// const mainNav = document.querySelector('main-nav-open');
-
-// if (searchButton) {
-//   document.addEventListener('click', function () {
-//     document.documentElement.classList.remove('main-nav-open');
-//   });
-// }
