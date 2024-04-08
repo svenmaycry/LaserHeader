2aff82c5bacab2f086ac7f180502b74ee249904c
SvenMayCry <svenmaycry@gmail.com>
2024-04-08 18:14:00 +0300

    Убраны px у 0.

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,111 +6,111 @@ const mainHeader = document.querySelector('.main-header');
 const headerContainer = document.querySelector('.main-header__container');
 const mainNavItemButton = document.querySelectorAll('.main-nav-item__button');
 
-//? При клике на поиск закрытие Меню-бургер + Сохранение класса lock.
+// При клике на поиск закрытие Меню-бургер + Сохранение класса lock.
 const closeBurgerMenu = () => {
-  if (body.classList.contains('main-nav-open')) {
-    body.classList.remove('main-nav-open', 'lock');
-  }
+    if (body.classList.contains('main-nav-open')) {
+        body.classList.remove('main-nav-open', 'lock');
+    }
 };
 
-//? При клике на поиск добавлять/убирать оверлей. Меняем класс lock у body.
+// При клике на поиск добавлять/убирать оверлей. Меняем класс lock у body.
 const overlayClassToggle = () => {
-  if (window.innerWidth <= 1279) {
-    overlay.classList.toggle('--active');
-  }
+    if (window.innerWidth <= 1279) {
+        overlay.classList.toggle('--active');
+    }
 };
 
-//? При клике на меню удаление класса --active оверлея.
+// При клике на меню удаление класса --active оверлея.
 const overlayClose = () => {
-  overlay.classList.remove('--active');
+    overlay.classList.remove('--active');
 };
 
-//? При клике на меню закрытие поиска + Сохранение класса lock.
+// При клике на меню закрытие поиска + Сохранение класса lock.
 const closeSearchWrapper = () => {
-  if (searchButton.classList.contains('--spoiler-active')) {
-    searchButton.classList.remove('--spoiler-active');
-    body.classList.remove('lock');
-  }
+    if (searchButton.classList.contains('--spoiler-active')) {
+        searchButton.classList.remove('--spoiler-active');
+        body.classList.remove('lock');
+    }
 };
 
-//? При клике на меню и поиск закрытие спойлеров мобильного меню.
+// При клике на меню и поиск закрытие спойлеров мобильного меню.
 const closeSpoilersContent = () => {
-  if (body.classList.contains('main-nav-open')) {
-    mainNavItemButton.forEach((oneButton) => {
-      if (oneButton.classList.contains('--spoiler-active')) {
-        oneButton.classList.remove('--spoiler-active');
-      }
-    });
-  }
+    if (body.classList.contains('main-nav-open')) {
+        mainNavItemButton.forEach((oneButton) => {
+            if (oneButton.classList.contains('--spoiler-active')) {
+                oneButton.classList.remove('--spoiler-active');
+            }
+        });
+    }
 };
 
-//? При скролле страницы показ и скрытие шапки.
+// При скролле страницы показ и скрытие шапки.
 let prevScrollPos = window.scrollY;
 const headerHeight = mainHeader.offsetHeight;
 const scrollThreshold = 200;
 
 const showAndHideHeader = () => {
-  let currentScrollPos = window.scrollY;
-  if (prevScrollPos > currentScrollPos) {
-    mainHeader.style.top = '0px';
-  } else {
-    if (currentScrollPos > scrollThreshold) {
-      mainHeader.style.top = `-${headerHeight}px`;
+    let currentScrollPos = window.scrollY;
+    if (prevScrollPos > currentScrollPos) {
+        mainHeader.style.top = '0px';
+    } else {
+        if (currentScrollPos > scrollThreshold) {
+            mainHeader.style.top = `-${headerHeight}px`;
+        }
     }
-  }
-  prevScrollPos = currentScrollPos;
+    prevScrollPos = currentScrollPos;
 };
 
-//? Изменить атрибут у контейнера шапки для изменения логики работы Спойлеров.
+// Изменить атрибут у контейнера шапки для изменения логики работы Спойлеров.
 const changeContainerAttribute = () => {
-  if (window.innerWidth <= 1279) {
-    headerContainer.removeAttribute('data-one-spoiler');
-  } else {
-    if (window.innerWidth >= 1279) {
-      headerContainer.setAttribute('data-one-spoiler', '');
+    if (window.innerWidth <= 1279) {
+        headerContainer.removeAttribute('data-one-spoiler');
+    } else {
+        if (window.innerWidth >= 1279) {
+            headerContainer.setAttribute('data-one-spoiler', '');
+        }
     }
-  }
 };
 
-//? Закрытие спойлера, оверлея и lock body, при ресайзе страницы.
+// Закрытие спойлера, оверлея и lock body, при ресайзе страницы.
 const closeSpoiler = () => {
-  if (!body.classList.contains('main-nav-open') && window.innerWidth <= 1279) {
-    mainNavItemButton.forEach((spoiler) => {
-      if (spoiler.classList.contains('--spoiler-active')) {
-        spoiler.classList.remove('--spoiler-active');
-        overlay.classList.remove('--active');
-        body.classList.remove('lock');
-      }
-    });
-  }
+    if (!body.classList.contains('main-nav-open') && window.innerWidth <= 1279) {
+        mainNavItemButton.forEach((spoiler) => {
+            if (spoiler.classList.contains('--spoiler-active')) {
+                spoiler.classList.remove('--spoiler-active');
+                overlay.classList.remove('--active');
+                body.classList.remove('lock');
+            }
+        });
+    }
 
-  if (body.classList.contains('main-nav-open') && window.innerWidth >= 1279) {
-    body.classList.remove('lock', 'main-nav-open');
-    mainNavItemButton.forEach((spoiler) => {
-      spoiler.classList.remove('--spoiler-active');
-    });
-  }
+    if (body.classList.contains('main-nav-open') && window.innerWidth >= 1279) {
+        body.classList.remove('lock', 'main-nav-open');
+        mainNavItemButton.forEach((spoiler) => {
+            spoiler.classList.remove('--spoiler-active');
+        });
+    }
 };
 
 const onSearchClick = () => {
-  closeSpoilersContent();
-  closeBurgerMenu();
-  overlayClassToggle();
+    closeSpoilersContent();
+    closeBurgerMenu();
+    overlayClassToggle();
 };
 
-const onIconMenuClick = (e) => {
-  closeSearchWrapper();
-  overlayClose(e);
-  closeSpoilersContent();
+const onIconMenuClick = (evt) => {
+    closeSearchWrapper();
+    overlayClose(evt);
+    closeSpoilersContent();
 };
 
 const onDocumentScroll = () => {
-  showAndHideHeader();
+    showAndHideHeader();
 };
 
 const onDocumentResize = () => {
-  changeContainerAttribute();
-  closeSpoiler();
+    changeContainerAttribute();
+    closeSpoiler();
 };
 
 searchButton.addEventListener('click', onSearchClick);
